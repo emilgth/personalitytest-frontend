0edad90daeee13a87ee020dd075e9f2d0816f684
Emil Hansen <emilgth@gmail.com>
2020-08-19 09:59:20 +0200

    added: + question list component + question service    + get list of questions    + get result

---
+++ src/app/question-list/question-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Question } from '../question';
+import { QuestionService } from '../question.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-question-list',
+  templateUrl: './question-list.component.html',
+  styleUrls: ['./question-list.component.css'],
+})
+export class QuestionListComponent implements OnInit {
+
+  questions: Question[];
+  answers = new Map();
+  result: string;
+
+  constructor(private questionService: QuestionService) { }
+
+  ngOnInit(): void {
+    this.getQuestions();
+  }
+
+  getQuestions(): void {
+    this.questionService.getQuestions().subscribe(questions => {
+      this.questions = questions;
+      questions.forEach(quest => {
+        this.answers.set(quest.question, 0);
+      });
+    });
+  }
+
+  setAnswer(score: number, question: string): void {
+    this.answers.set(question, score);
+  }
+
+  getResult(): void {
+    this.questionService.getResult(this.answers).subscribe(result => {
+      this.result = result;
+      console.log(this.result);
+    });
+  }
+}
---
+++ src/app/question.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Question } from './question';
+import { catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuestionService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  private questionUrl = 'http://localhost:8080/api/questions';
+  private resultUrl = 'http://localhost:8080/api/result';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  getQuestions(): Observable<Question[]> {
+    return this.http.get<Question[]>(this.questionUrl).pipe(
+      catchError(this.handleError<Question[]>('getQuestions', [])),
+    );
+  }
+
+  getResult(answers: Map<string, number>): Observable<string> {
+    let answersList = Array.from(answers.values());
+    answersList = answersList.filter(answer => answer !== 0);
+    console.log(answersList);
+    return this.http.post<string>(this.resultUrl, answersList, this.httpOptions).pipe(
+      catchError(this.handleError<string>('getResult')),
+    );
+  }
+
+  private handleError<T>(operation: string = 'operation', result?: T): (error: any) => Observable<T> {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
